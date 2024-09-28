52fa6f0dc815af0b35859e9d18973726b20e1540
anaseem80 <anaseemamin@gmail.com>
2024-09-28 05:26:03 +0300

    Introduction to CS

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// namespace declaration
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your name");
+
+        string userName = Console.ReadLine();
+
+        Console.WriteLine("Hello {0}", userName);
+
+        //Console.WriteLine("Welcome back " + userName + "!");
+    }
+}
